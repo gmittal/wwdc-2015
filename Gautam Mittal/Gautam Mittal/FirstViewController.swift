93314b1026bf11712f4cb7325f3b6fdfa7874587
gmittal <gautam@mittal.net>
2015-04-21 18:23:01 -0700

    more progress

--- Gautam Mittal/Gautam Mittal/FirstViewController.swift
+++ Gautam Mittal/Gautam Mittal/FirstViewController.swift
@@ -1,22 +1,27 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  Gautam Mittal
 //
-//  Created by Gautam Mittal on 4/17/15.
+//  Created by Gautam Mittal on 4/21/15.
 //  Copyright (c) 2015 Gautam Mittal. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.edgesForExtendedLayout = UIRectEdge();
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        
         // Dispose of any resources that can be recreated.
     }
 
---
+++ Gautam Mittal/Gautam Mittal/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Gautam Mittal
+//
+//  Created by Gautam Mittal on 4/21/15.
+//  Copyright (c) 2015 Gautam Mittal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
