776d31bbcd6e5f51926d894fd5ca39302d2a44de
gmittal <gautam@mittal.net>
2015-04-25 18:35:20 -0700

    finished basic MVP, except for playground

--- Gautam Mittal/Gautam Mittal/PlaygroundViewController.swift
+++ Gautam Mittal/Gautam Mittal/PlaygroundViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class PlaygroundViewController: UIViewController {
 
+    @IBAction func buildCodeTapped(sender: AnyObject) {
+        resignFirstResponder();
+        view.endEditing(true);
+    }
  
     
     override func viewDidLoad() {
@@ -17,8 +21,13 @@ class PlaygroundViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         self.edgesForExtendedLayout = UIRectEdge();
-//        self.title = "Playground"
-//        self.navigationItem.backBarButtonItem?.title = "Back";
+
+        var alertView = UIAlertView();
+        alertView.addButtonWithTitle("Let's Code!");
+        alertView.delegate = self
+        alertView.title = "Playground";
+        alertView.message = "Welcome to my playground! You're about to get the opportunity to write code in my two favorite languages: Swift, and JavaScript. By default, the editor is set to Swift, but double tapping the text view should switch the language to JavaScript. Hit the 'Run' button in the upper right-hand corner to compile.";
+        alertView.show();
 
     }
 
